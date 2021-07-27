5d8b7a06f92280618b74b44b687368d07e679175
Marco Santana <marcos.sinsan@gmail.com>
2021-07-27 19:10:19 -0300

    Ref: Modificando arquivo principal e add novo arquivo.

---
+++ python/introducao_a_python/variaveis.py
@@ -0,0 +1,27 @@
+'''
+Arquivo: variaveis.py
+Data: 2/07/2021
+Descrição: Manipulando variáveis através de operadores aritmáticos e interação com usuário.
+Autor: Marcos Santana
+'''
+
+# Variáveis
+
+a = 10
+b = 5
+
+# Calculos aritméticos
+
+soma = a + b
+subtracao = a - b
+multiplicacao = a * b
+divisao = a / b
+resto = a % b
+
+# Imprimindo variáveis
+
+print('Soma: %s' %(soma))
+print('Subtração: %s' %(subtracao))
+print('Multiplicação: %s' %(multiplicacao))
+print('Divisão: %.0f' %(divisao))
+print('Resto: %s' %(resto))
