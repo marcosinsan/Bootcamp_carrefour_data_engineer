acc0cfb5724ce02397ec756581449164dc2444a8
Marco Santana <marcos.sinsan@gmail.com>
2021-07-26 21:12:55 -0300

    Arquivo de introducao ao Python

---
+++ python/introducao_a_python/main.py
@@ -0,0 +1,18 @@
+'''
+Arquivo: main.py
+Data: 26/07/2021
+Descrição: Introdução à linguagem Python
+Autor: Marcos Santana
+'''
+
+## Criando primeiro programa
+
+print('Hello World!')
+
+## Fazendo calculos com python
+
+a = 5
+b = 10
+c = a + b
+
+print(c)
