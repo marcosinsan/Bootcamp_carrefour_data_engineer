25c401ce9496d562f5a6384cbb68b3e358413789
Marco Santana <marcos.sinsan@gmail.com>
2021-07-27 19:46:24 -0300

    Ref: Add codigo de intercao com usuario.

--- python/introducao_a_python/variaveis.py
+++ python/introducao_a_python/variaveis.py
@@ -20,8 +20,23 @@ resto = a % b
 
 # Imprimindo variáveis
 
-print('Soma: %s' %(soma))
-print('Subtração: %s' %(subtracao))
-print('Multiplicação: %s' %(multiplicacao))
-print('Divisão: %.0f' %(divisao))
-print('Resto: %s' %(resto))
+print('Soma: %s' % (soma))
+print('Subtração: %s' % (subtracao))
+print('Multiplicação: %s' % (multiplicacao))
+print('Divisão: %.0f' % (divisao))
+print('Resto: %s' % (resto))
+
+# Criando um interção com usuário
+
+numero = int(input('Digite um número: '))
+numero2 = int(input('Digite outro número: '))
+
+soma = numero + numero2
+subtracao = numero - numero2
+multiplicacao = numero * numero2
+divisao = numero / numero2
+potencia = numero ** numero2
+resto = numero % numero2
+
+print(f'Soma: {soma},\n Subtração: {subtracao},\n Multiplicação: {multiplicacao},\n Divisão: {divisao},\n Potência:'
+      f' {potencia},\n Resto: {resto}')
